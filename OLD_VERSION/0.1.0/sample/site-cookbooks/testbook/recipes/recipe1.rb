88c8e83250e95e18ddd5b0f4fe530aee75d63ac3
Masaya Aoyama <tennis.tenmasa@gmail.com>
2015-11-02 02:26:32 +0900

    [remove] Remove version 0.1.0 files
    These files are not necessary.

--- OLD_VERSION/0.1.0/sample/site-cookbooks/testbook/recipes/recipe1.rb
+++
@@ -1,8 +0,0 @@
-package "httpd" do
-    action :install
-    version node["httpd"]["version"]
-end
-service "httpd" do
-    supports :status => true, :restart => true, :reload => true
-    action [:disable , :start]
-end
--- OLD_VERSION/0.1.0/src/util/pair.rb
+++
@@ -1,8 +0,0 @@
-class Pair
-    attr_accessor :classname, :taskname
-
-    def initialize(classname, taskname)
-        @classname = classname
-        @taskname = taskname
-    end
-end
